47360f1d2053f2d79ac508e18f33f13bdac49c28
Pablito V.R <pablitovr007@gmail.com>
2021-01-29 19:48:51 +0100

    Pokedex acabada

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,22 +1,60 @@
 const URL_BASE = 'https://pokeapi.co/api/v2/pokemon/';
 const URL_BASE_IMG = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-function findPokemon(){
+async function findPokemon(){
     var HTMLinputId = document.getElementById('inputPokemonID');
     const pokemonId = HTMLinputId.value;
     
     if(pokemonId.length > 0){
         // Vamos a llamar al API
 
-        const response = fetch(URL_BASE + pokemonId).then(
+        const response = await fetch(URL_BASE + pokemonId).then(response => response.json()).catch(error => {
+            alert("Error buscando el pokemon con id: " + pokemonId);
+            console.log(error);
+        });
 
-            (response) => {
-                console.log(response.json());
-            }
+        if(response){
+            localStorage.setItem('datosPokemon', JSON.stringify(response));
 
-        );
+            localStorage.setItem('pokemonId', pokemonId);
 
-    }else{
-        alert("El campo de texto está vacío");
+            window.location.href = 'detalle.html';
+        }
+
+        console.log(response);
+
+    } else {
+        alert('Introduzca un identificador válido');
+    }
+
+}
+
+function cargarPokemonData(){
+    if(localStorage.getItem('datosPokemon') == null){
+        alert('Pokemon no encontrado');
+
+    } else{
+        var contentHTML = document.getElementsByClassName('content')[0];
+
+        var pokemon = JSON.parse(localStorage.getItem('datosPokemon'));
+        var nombrePokemon = pokemon.name;
+
+        var HTMLnombre = document.getElementById('pokemonNombre');
+        HTMLnombre.innerHTML += nombrePokemon.toLocaleUpperCase();
+
+        var HTMLimagen = document.getElementById('pokemonImagen');
+        HTMLimagen.src = URL_BASE_IMG + (localStorage.getItem('pokemonId')) + '.png';
+
+        var altura = pokemon.height;
+        var HTMLaltura = document.createElement('h5');
+        HTMLaltura.innerHTML = 'Altura: ' + altura;
+        contentHTML.appendChild(HTMLaltura);
+
+        var peso = pokemon.weight;
+        var HTMLpeso = document.createElement('h5');
+        HTMLpeso.innerHTML = 'Peso: ' + peso;
+        contentHTML.appendChild(HTMLpeso);
+
+        console.log(nombrePokemon);
     }
 }
